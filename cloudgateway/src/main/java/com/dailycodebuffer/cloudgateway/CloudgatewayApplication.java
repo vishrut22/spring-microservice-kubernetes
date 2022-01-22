432809f72b944bd5c648f93723d89f66327b67ad
Vishrut Majmudar <v.h.majmudar@gmail.com>
2022-01-22 16:55:10 +0530

    Cloud Gateway Configuration

---
+++ cloudgateway/src/main/java/com/dailycodebuffer/cloudgateway/CloudgatewayApplication.java
@@ -0,0 +1,31 @@
+package com.dailycodebuffer.cloudgateway;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.common.ratelimiter.configuration.RateLimiterConfigCustomizer;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudgatewayApplication.class, args);
+	}
+
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(10)).build()).build());
+	}
+
+}
