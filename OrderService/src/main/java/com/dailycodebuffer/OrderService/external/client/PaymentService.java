8cb167d22cbd321e53f951bb3188afcb9184bfde
Vishrut Majmudar <v.h.majmudar@gmail.com>
2022-08-27 18:05:08 +0530

    Feign circuit break

--- OrderService/src/main/java/com/dailycodebuffer/OrderService/external/client/PaymentService.java
+++ OrderService/src/main/java/com/dailycodebuffer/OrderService/external/client/PaymentService.java
@@ -1,15 +1,22 @@
 package com.dailycodebuffer.OrderService.external.client;
 
+import com.dailycodebuffer.OrderService.exception.CustomException;
 import com.dailycodebuffer.OrderService.external.request.PaymentRequest;
 import com.dailycodebuffer.OrderService.external.response.ProductResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@CircuitBreaker(name = "external" , fallbackMethod = "fallback")
 @FeignClient( name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
     @RequestMapping(method = RequestMethod.POST)
     public void doPayment(@RequestBody PaymentRequest paymentRequest);
+
+    default void fallback(Exception e) {
+        throw new CustomException("UNAVAILABLE","Payment service not available.",500);
+    }
 }
--- OrderService/src/main/java/com/dailycodebuffer/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/dailycodebuffer/OrderService/external/client/ProductService.java
@@ -1,12 +1,16 @@
 package com.dailycodebuffer.OrderService.external.client;
 
+import com.dailycodebuffer.OrderService.exception.CustomException;
 import com.dailycodebuffer.OrderService.external.response.ProductResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external" , fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
     @RequestMapping(method = RequestMethod.GET, value ="{id}")
@@ -15,4 +19,7 @@ public interface ProductService {
     @RequestMapping(method = RequestMethod.PUT, value ="/reduceQuantity/{id}/")
     public void reduceQuantity(@PathVariable("id") long id, @RequestParam("quantity") long quantity);
 
+    default void fallback(Exception e) {
+        throw new CustomException("UNAVAILABLE","Product service not available.",500);
+    }
 }
